e74d510f15f7b14260f01b04622667f9b1342552
VERMON013 <javierms5@hotmail.com>
2022-03-28 09:46:42 +0200

    Nuevo proyecto de angular desde cero (opciones por defecto al crearlo)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ApexchartsAngularTest
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.16.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppComponent } from './app.component';
-import { LineChartComponent } from './components/line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LineChartComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgApexchartsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
