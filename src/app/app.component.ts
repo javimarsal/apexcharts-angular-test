b3843406c8a0464ef27b9cae054f5b25db0a1e50
VERMON013 <javierms5@hotmail.com>
2022-03-28 10:28:02 +0200

    Añadido los scripts y el import del módulo NgApexchartsModule, según la página oficial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apexcharts-angular-test';
+  title = 'Apexcharts angular-test';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
